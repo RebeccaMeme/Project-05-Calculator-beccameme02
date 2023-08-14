96c3a56fb9152d80ba9ba65564fb9600721119c7
Rebecca <beccameme02@gmail.com>
2023-08-14 16:39:43 +0200

    click.event

--- .lesson/instructions.md
+++ .lesson/instructions.md
@@ -1,23 +1,25 @@
- # Instructions
+# Instructions
 
-## Screens
-* The bottom input (*can be selected with id value* `input`) holds the current input number.
-* The upper label (*can be selected with id value* `calcul`) holds the current computation string as the user types. Operators are preceeded and followed by a single space.
-* A number is added to the label after pressing an operator.
-* Le calcul ne se fait que quand on apuie sur `=`ou `%`. `
-* For example, the computation `23 × 2 = 46`, will have `2` in the input and the string `23 × 2` in the text above the input field.
+## Écrans
+* L'entrée du bas (*peut être sélectionnée avec la valeur d'identification* `input`) contient le numéro de l'entrée en cours.
+* L'étiquette supérieure (*peut être sélectionnée avec la valeur d'identification* `calcul`) contient la chaîne de calcul actuelle au fur et à mesure que l'utilisateur la tape. Les opérateurs sont précédés et suivis d'un espace.
+* Un nombre est ajouté à l'étiquette après avoir appuyé sur un opérateur.
+* Le calcul ne se fait que quand on appuie sur `=`ou `%`. `
+* Par exemple, le calcul `23 × 2 = 46`, aura `2` dans l'entrée et la chaîne `23 × 2` dans le texte au-dessus du champ de saisie.
 
-## Buttons
-Buttons are the ***ONLY WAY*** to input data and work as they do in a physical calculator as follows:
-- Pressing the numpad adds the digit on the input value
-- The dot `.` can be in a string only once and can only follow a digit
-- Zero cannot be repeated at the beginning of a number eg. `0000` is not valid input value
-- Pressing the `+/-` buttong changes the sign of the current input number
-- Pressing the `%` button returns the calculations result as a percentage. i.e. divides it a 100.
-- `AC` resets everything, and `C` clears the input alone.
-- When displaying the result of an operation, an equal sign `=` is added to the input string and the results is diplayed in the input field.
+## Boutons
+Les boutons sont la ***SULE FAÇON*** d'entrer des données et fonctionnent de la même manière que dans une calculatrice physique, comme suit :
+- Une pression sur le pavé numérique permet d'ajouter un chiffre à la valeur saisie.
+- Le point `.` ne peut figurer qu'une seule fois dans une chaîne de caractères et ne peut suivre qu'un chiffre.
+- Le zéro ne peut pas être répété au début d'un nombre, par exemple `0000` n'est pas une valeur d'entrée valide.
+- En appuyant sur le bouton `+/-`, on change le signe du nombre saisi.
+- En appuyant sur le bouton `%`, le résultat du calcul est exprimé en pourcentage, c'est-à-dire qu'il est divisé par 100.
+- La touche `AC` remet tout à zéro, et la touche `C` efface l'entrée seule.
+- Pour afficher le résultat d'une opération, le signe égal `=` est ajouté à la chaîne de saisie et le résultat s'affiche dans le champ de saisie.
 
 ## Bonus
-- ...
-- Add a `00`
-- 
+- .de..
+- Ajouter un `00`
+-
+
+Traduit avec www.DeepL.com/Translator (version gratuite)
--- script.js
+++ script.js
@@ -1,108 +1,62 @@
-import{calculate} from "./calculator";
+//import {calculate} from "./calculator";
 
 // TODO: Faire la manipulation du DOM dans ce fichier
-// ttt
-let lowerInput = document.getElementById("input");
-let currentNumber = lowerInput.value;
+//ttt
+let inputElement = document.getElementById("input");
+let calculElement = document.getElementById("calcul");
+let numericButtons = document.querySelectorAll(".numpad");
+numericButtons.forEach((button)=> {
+  // console.log(button.textContent);
+  button.addEventListener("click",(e)=> {
+  //  console.log(e.target.textContent);
+  inputElement.value += e.target.textContent
+  })
+})
+let equalsButton = document.getElementById("equals");
+let percentageButton = document.getElementById("percentage");
 
-console.log(currentNumber); 
-let label = document.getElementById("calcul");
-document.addEventListener("keyup", function(event) {
-    let key = event.key;
-    if (key.match(/[+\-*/0-9]/)) {
-        label.textContent += ` ${key} `;
-    }
-  });
-  let operatorButton = document.querySelector(".operator");
-let numberButton = document.querySelector(".number");
-operatorButton.addEventListener("click", function() {
-    let label = document.getElementById("label"); 
-  
-    if (label.textContent === "") {
-      label.textContent += numberButton.value; 
-    } else {
-      label.textContent += " " + numberButton.value; 
-    }
-  });
-  let equalsButton = document.getElementById("equals-button");
-let percentageButton = document.getElementById("percentage-button");
+// numericButtons.forEach(function(button){
+//   button.addEventListener("click", function(){
 
-equalsButton.addEventListener("click", function() {
-    calculateResult();
-});
-percentageButton.addEventListener("click", function() {
-    calculatePercentage();
-});
-function calculate() {
-  let inputValue = document.getElementById("input").value;
-  let numberValue = parseInt(inputValue);
-  let result = numberValue *2;
-   console.log("Le résultat est : " + result);
-}
-let userInput = document.getElementById("user-Input");
-document.addEventListener("keydown", function(event) {
-    let key = event.key;
-   if (/^[0-9]$/.test(key)) {
-      let number = parseInt(key);
-      userInput.value += number;
-    };
-});
-function checkString(str) {
-  let regex = /^\d+\.\d+$/;
-if (regex.test(str)) {
-    console.log("La chaîne respecte les conditions requises.");
-  } else {
-    console.log("La chaîne ne respecte pas les conditions requises.");
-  }
-}
-function evaluateExpression() {
-  let input = document.getElementById("input").value;
+//     let digit = this.textContent;
+//     if (!(digit === "0" && input === "")){
+//       input += digit;
+//     }
+//   });
+// });
+// let currentCalculation = "";
+// function updateInput(value){
+//   inputElement.value += value;
+//   function updateCalculation(value){
+//     currentCalculation += value;
+//     calculElement.textContent = currentCalculation;
+//   }
+//   function calculate(){
+//     result;
+//     try{
+//       result = eval(currentCalculation);
+//       inputElement.value = result;
+//       currentCalculation = "";
+//       calculElement.textContent = "";
+//     } catch (error) {
+//       // Gestion des erreurs lors du calcul
+//       console.error(error);
+//     }
+//  }
+// }
+// let input = "";
 
-  // Vérifier si l'entrée commence par un zéro suivi d'autres chiffres
-  if (/^0[0-9]/.test(input)) {
-    console.log("Valeur d'entrée invalide !");
-    return;
-  }
-  let result = eval(input);
-  document.getElementById("output").innerHTML = "Résultat : " + result;
-}
-document.getElementById("calculate-button");
-document.addEventListener("click", evaluateExpression);
-let plusButton = document.getElementById("plus-button");
-let minusButton = document.getElementById("minus-button");
-plusButton.addEventListener("click", function() {
-  currentNumber = -currentNumber;
-  updateDisplay(currentNumber);
-});
-minusButton.addEventListener("click", function() {
-  currentNumber = -currentNumber;
-  updateDisplay(currentNumber);
-});
-percentageButton.onclick = function() {
-  let resultElement = document.getElementById("result");
-  let resultValue = parseFloat(resultElement.value);
-  // Calculer le pourcentage en divisant par 100
-  let percentageResult = resultValue / 100;
-  resultElement.value = percentageResult;
-};
-let currentInput = "";
-let previousResult = 0;
-function AC() {
-  currentInput = "";
-  previousResult = 0;
-}
-function C() {
-  currentInput = "";
-}
-document.getElementById("reset-button").addEventListener("click", AC);
-document.getElementById("clear-button").addEventListener("click", C);
-function calculate() {
-  let inputEquals = document.getElementById("equals");
-  // Lire la valeur actuelle du champ de saisie
-  let inputValue = inputEquals.value;
-  let result = eval(inputValue);
-  // Ajouter le signe égal (=) à la chaîne d'entrée et afficher le résultat dans le champ de saisie
-  inputEquals.value = inputValue + " = " + result;
-}
+// let plusMinusButton = document.getElementById("plus-Minus-Button");
+// plusMinusButton.addEventListener("click", function(){
+//   if (input !== ""){
+//     if(input[0] === "-"){
+//       input = input.slice(1);
+//     } else{
+//       input = "-" + input;
+//     }
+//   }
+// })
+
+
 
         
